11b9baad4f98a1d4538d2dde02dfeeacb12a29aa
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2025-03-03 23:43:35 -0700

    Instantiate UML vars and getters and setters for tag
    Tag will help tag files

---
+++ src/main/java/com/cloudnotesmanager/Tag.java
@@ -0,0 +1,32 @@
+package com.cloudnotesmanager;
+
+public class Tag {
+    private int id;
+    private int fileId;
+    private String name;
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(int fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
