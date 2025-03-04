4f39d8274d5f98d6f591c396a850b003d9b1f6f8
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2025-03-03 23:43:01 -0700

    Instatiate UML vars and getters and setters

---
+++ src/main/java/com/cloudnotesmanager/File.java
@@ -0,0 +1,59 @@
+package com.cloudnotesmanager;
+
+public class File {
+    private int id;
+    private String name;
+    private String path;
+    private int userId;
+    private int folderId;
+    private String uploadTimestamp;
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getFolderId() {
+        return folderId;
+    }
+
+    public void setFolderId(int folderId) {
+        this.folderId = folderId;
+    }
+
+    public String getUploadTimestamp() {
+        return uploadTimestamp;
+    }
+
+    public void setUploadTimestamp(String uploadTimestamp) {
+        this.uploadTimestamp = uploadTimestamp;
+    }
+}
