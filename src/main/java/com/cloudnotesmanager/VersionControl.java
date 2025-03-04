6ee9d580cacaed3b9321e0d23230e6b3351ac23c
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2025-03-03 23:47:37 -0700

    Create VersionControl.java
    Instantiates variables needed to reverse changed and have older versions of documents. I want to make it so I can use services like google docs to open up a document though

---
+++ src/main/java/com/cloudnotesmanager/VersionControl.java
@@ -0,0 +1,41 @@
+package com.cloudnotesmanager;
+
+public class VersionControl {
+    private int id;
+    private int fileId;
+    private String version;
+    private String timestamp;
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(int fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
