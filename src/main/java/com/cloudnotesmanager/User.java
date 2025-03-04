4db956712bd1a5d99b2a411b4928b41d2455cd21
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2025-03-03 23:43:59 -0700

    HElps create user from uml
    More password hash methods need to be made

---
+++ src/main/java/com/cloudnotesmanager/User.java
@@ -0,0 +1,41 @@
+package com.cloudnotesmanager;
+
+public class User {
+    private int id;
+    private String username;
+    private String passwordHash;
+    private String email;
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
